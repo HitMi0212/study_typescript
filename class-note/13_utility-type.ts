7a04606ac02b91cbd4e83419e11e62bb2b427b6f
Seung Jun <pache31@naver.com>
2023-08-23 20:56:30 +0900

    유틸리티타입 - Partial

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -6,10 +6,9 @@ interface Product {
     stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API함수
+// 1. 상품 목록을 받아오기 위한 API함수
 function fetchProducts(): Promise<Product[]> {
     // ..
-
 }
 
 // interface ProductDetail {
@@ -18,8 +17,67 @@ function fetchProducts(): Promise<Product[]> {
 //     price: number;
 // }
 
+// 2. 특정 상품의 상세 정보
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 
 function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
-
+    // ..
 }
+
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+type UpdateProduct = Partial<Product>
+
+// 3. 특정 상품 정보를 업데이트(갱신)하는 함수
+function updateProductItem(productItem: Partial<Product>) {
+    // ..
+}
+
+// 4. 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+    username: string;
+    email: string;
+    profilePhotoUrl: string;
+}
+
+// interface UserProfileUpdate {
+//     username?: string;
+//     email?: string;
+//     profilePhotoUrl?: string;
+// }
+
+// #1
+type UserProfileUpdate = {
+    username?: UserProfile['username'];
+    email?: UserProfile['email'];
+    profilePhotoUrl?: UserProfile['profilePhotoUrl'];
+}
+
+// #2 - Mapped Type
+type UserProfileUpdate = {
+    [p in 'username' | 'email' | 'profilePhotoUrl']?: UserProfile[p]
+}
+
+// #3
+type UserProfileKeys = keyof UserProfile
+
+type UserProfileUpdate = {
+    [p in keyof UserProfile]?: UserProfile[p]
+}
+
+// #4 - Partial의 구현체와 동일
+type Subset<T> = {
+    [p in keyof T]?: T[p]
+}
+
+// #5
+type UserProfileUpdate = Partial<UserProfile>
+
+
+
